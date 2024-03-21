273448e9ef3ab53f6c54cd65cf045c7fca716548
Snehangshu Biswas <dev.snehangshu@gmail.com>
2024-03-21 22:12:16 +0530

    added dotenv

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import products from './data/products'
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
 	res.send('Hello from server')
