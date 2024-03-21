0839cf18dcd2aaee53bdb26445929fa6fe6379ab
Snehangshu Biswas <dev.snehangshu@gmail.com>
2024-03-21 10:48:49 +0530

    added product details screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,11 +10,13 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomeScreen from './screens/HomeScreen.tsx'
+import ProductScreen from './screens/ProductScreen.tsx'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
 			<Route index={true} path='/' element={<HomeScreen />} />
+			<Route path='/product/:id' element={<ProductScreen />} />
 		</Route>
 	)
 )
