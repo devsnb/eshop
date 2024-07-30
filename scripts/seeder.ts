d4ceda2363157e52fafe53ac957f06e000b5c7bf
Snehangshu Biswas <dev.snehangshu@gmail.com>
2024-07-30 19:38:30 +0530

    moved seeder.ts to scripts

--- scripts/seeder.ts
+++ scripts/seeder.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
-import users from './data/users'
-import products from './data/products'
-import User from './models/userModel'
-import Product from './models/productModel'
-import Order from './models/orderModel'
-import connectDB from './config/db'
+import users from '../data/users'
+import products from '../data/products'
+import User from '../models/userModel'
+import Product from '../models/productModel'
+import Order from '../models/orderModel'
+import connectDB from '../config/db'
 
 dotenv.config()
 
